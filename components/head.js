2a52615e2310ae257321fa87a15ffed2601d0506
aaronhugaert <74785976+aaronhugaert@users.noreply.github.com>
2020-11-24 15:06:34 +0100

    Update head.js

--- components/head.js
+++ components/head.js
@@ -1,22 +1,9 @@
 import NextHead from 'next/head';
 import { string } from 'prop-types';
 
-const defaultDescription = '';
-const defaultKeywords = '';
-const defaultOGURL = '';
-const defaultOGImage = '';
-
 const Head = (props) => (
   <NextHead>
-    <meta charSet="UTF-8" />
     <title>Snappy Recipes</title>
-    <meta name="description" content={props.description || defaultDescription} />
-    <meta name="keywords" content={props.keywords || defaultKeywords} />
-    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
-    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-    <link rel="shortcut icon" href="/static/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#000000" />
   </NextHead>
 );
 
